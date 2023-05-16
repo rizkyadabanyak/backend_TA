b759c66a44a0503da88fc7e69275f1f82729b44b
rizky <rizkypr@rnd.ts.co.id>
2023-05-16 07:51:24 +0700

    test

--- database/config.js
+++ database/config.js
@@ -1,9 +1,14 @@
 const Sequelize = require('sequelize')
 
 
-const db = new Sequelize('skirpsi', 'postgres', '28112000', {
-    host: "localhost",
+const db = new Sequelize('hapi', 'postgres', '28112000', {
+    host: "hapi.cdzruycabei0.us-east-1.rds.amazonaws.com",
     dialect: 'postgres'
 });
+
+// const db = new Sequelize('skirpsi', 'postgres', '28112000', {
+//     host: "localhost",
+//     dialect: 'postgres'
+// });
 
 module.exports = { db }
