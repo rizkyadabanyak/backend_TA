fc22a16fe4538189c54e48ee8b73828d9cdec4f7
rizky <rizkypr@rnd.ts.co.id>
2023-05-24 18:54:37 +0700

    test

--- database/config.js
+++ database/config.js
@@ -1,10 +1,6 @@
 const Sequelize = require('sequelize')
 
-
-// const db = new Sequelize('hapi', 'postgres', '28112000', {
-//     host: "hapi.cdzruycabei0.us-east-1.rds.amazonaws.com",
-//     dialect: 'postgres'
-// });
+require('dotenv').config()
 
 const db = new Sequelize(process.env.DATABASE, process.env.USERNAME_DB, process.env.PASSWORD_DB, {
     host: process.env.HOST_DB,
